a14cfa35a0e360f8036c0026008d414e96fa3b77
Martin A. Ortega <martin.ortega@apifiedsignature.com>
2024-01-06 00:14:24 -0600

    Adds source code to project.

---
+++ src/TestRadComboBox/TestRadComboBox/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestRadComboBox
+{
+    public class Product
+    {
+        public int Product_ID { set; get; }
+        public string ProductCode { set; get; }
+        public string ProductName { set; get; }
+    }
+}
