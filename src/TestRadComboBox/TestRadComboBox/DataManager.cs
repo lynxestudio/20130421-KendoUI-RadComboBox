318b99e162b8523b169402a2671fe77f6f9d5182
Martin A. Ortega <lynxestudio@outlook.com>
2024-08-18 12:21:09 -0600

    Adds source code to the project

---
+++ src/TestRadComboBox/TestRadComboBox/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+using System.Data;
+using System.Text;
+
+namespace TestRadComboBox
+{
+public class DataManager
+{
+public List<Product> GetProducts(ProductFilters filters) {
+    StringBuilder commandText = new StringBuilder("SELECT product_id, ")
+    .Append("product_code, ")
+    .Append("product_name ")
+    .Append("FROM Products ")
+    .AppendFormat("WHERE product_name Like '%{0}%'",filters.ProductName.ToUpper());
+    List<Product> productList = null;
+    Product p = null;
+    using(NpgsqlDataReader reader = PostgreSQLCommand.GetReader(commandText.ToString(),
+        null,
+        CommandType.Text)){
+        productList = new List<Product>();
+        while (reader.Read()) {
+            p = new Product();
+            p.Product_ID = Convert.ToInt32(reader["product_id"]);
+            p.ProductCode = reader["product_code"].ToString();
+            p.ProductName = reader["product_name"].ToString();
+            productList.Add(p);
+        } 
+    }
+    return productList;
+}
+}
+}
---
+++ src/TestRadComboBox/TestRadComboBox/PostgreSQLDataBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Configuration;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace TestRadComboBox
+{
+internal sealed class PostgreSQLDataBase
+{
+static NpgsqlConnection _conn = null;
+static PostgreSQLDataBase Instance = null;
+string ConnectionString = null;
+private PostgreSQLDataBase()
+{
+    try
+    {
+        ConnectionString = ConfigurationManager.
+            ConnectionStrings["myinvoices"].ConnectionString;
+    }
+    catch (Exception ex)
+    {
+        Logger.LogWriteError(ex.Message);
+        throw ex;
+    }
+}
+private static void CreateInstance()
+{
+    if (Instance == null)
+    { Instance = new PostgreSQLDataBase(); }
+}
+public static PostgreSQLDataBase GetInstance()
+{
+    if (Instance == null)
+        CreateInstance();
+    return Instance;
+}
+public NpgsqlConnection GetConnection()
+{
+    try
+    {
+        _conn = new NpgsqlConnection(ConnectionString);
+        _conn.Open();
+        return _conn;
+    }
+    catch (Exception ex)
+    {
+        Logger.LogWriteError(ex.Message);
+        throw ex;
+    }
+}
+}
+}
